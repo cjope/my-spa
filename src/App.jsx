85c02868b7c639b75ce8bf829bb6c289ba71a77d
Craig Jope <craigjope@gmail.com>
2025-08-04 08:59:39 -0600

    add mui, navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
+import SimpleBottomNavigation from "./BottomNavigation";
+import TopAppBar from "./assets/TopAppBar.jsx";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
+      <TopAppBar />
       <div>
         <a href="http://craigjope.com" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,13 +20,12 @@ function App() {
         </a>
       </div>
       <h1>Getting things set up here...</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Testing Changes</p>
-      </div>
-    </>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>Testing Changes</p>
+      <SimpleBottomNavigation />
+    </div>
   );
 }
 
