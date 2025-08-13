f301416a5400b2b9c054ee6b032215e0319698e3
cjope <craigjope@gmail.com>
2025-08-13 10:47:26 -0600

    Install router dom and create route for sonarr

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import SimpleBottomNavigation from "./BottomNavigation";
@@ -8,34 +9,50 @@ import Projects from "./Projects";
 import Sonarr from "./Sonarr.jsx";
 import Calendar from "./Calendar.jsx";
 
+function Home({ count, setCount }) {
+  return (
+    <>
+      <div>
+        <a href="http://craigjope.com" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="http://craigjope.com" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+      <h1>Getting things set up here...</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>Testing Changes</p>
+    </>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="AppLayout">
-      <div className="TopBar">
-        <TopAppBar />
-      </div>
-      <div className="ContentArea">
-        <div>
-          <a href="http://craigjope.com" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="http://craigjope.com" target="_blank">
-            <img src={reactLogo} className="logo" alt="React logo" />
-          </a>
+    <Router>
+      <div className="AppLayout">
+        <div className="TopBar">
+          <TopAppBar />
         </div>
-        <h1>Getting things set up here...</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Testing Changes</p>
-        <Sonarr />
+        <div className="ContentArea">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home count={count} setCount={setCount} />}
+            />
+            <Route path="/sonarr" element={<Sonarr />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </div>
+        <div className="BottomBar">
+          <SimpleBottomNavigation />
+        </div>
       </div>
-      <div className="BottomBar">
-        <SimpleBottomNavigation />
-      </div>
-    </div>
+    </Router>
   );
 }
 
